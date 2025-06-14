ddf1752d6499c048f3402c3bc3221d1b496ac34c
williamq-cloud <williamqin2011@gmail.com>
2025-06-14 11:20:27 -0700

    Put everything in!

--- BasicPongJava/src/main/Ball.java
+++ BasicPongJava/src/main/Ball.java
@@ -5,7 +5,11 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class Ball extends Rectangle{
+public class Ball extends Rectangle {
+	Random random;
+	int xVelocity;
+	int yVelocity;
+	
 	Ball() {
 		
 	}
